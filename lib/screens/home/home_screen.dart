d1f24811993b3a4537ed4642a9c3c1eee27d55d2
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2020-06-27 20:11:47 +0530

    Folder structure and basic layout

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello World !!!"),
+        ),
+      ),
+    );
+  }
+}
