6dc0d095e8819ce7165dbe7a43960ea0ba8ff2d4
Aayush Bisen <aayush23101998@outlook.com>
2020-06-28 23:03:43 +0530

    added color pallete

---
+++ lib/helpers/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/painting.dart';
+
+class ColorPallete {
+  // Provided Pallete
+  static const Color background = Color(0xFF060645);
+  static const Color cardBackground = Color(0xFF010218);
+  static const Color color3 = Color(0xFF05F29B);
+  static const Color color4 = Color(0xFF115664);
+  static const Color cardHeading = Color(0xFF12A1A3);
+  static const Color color6 = Color(0xFF146B8D);
+  static const Color color7 = Color(0xFF079476);
+}
+
+// Given on Dribble
+// https://dribbble.com/colors/079476
+// https://dribbble.com/colors/146B8D
+// https://dribbble.com/colors/12A1A3
+// https://dribbble.com/colors/115664
+// https://dribbble.com/colors/05F29B
+// https://dribbble.com/colors/010218
+// https://dribbble.com/colors/060645
