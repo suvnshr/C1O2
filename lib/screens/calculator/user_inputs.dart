087a5b3c948e84c91b034c0f03ccf992a05f3424
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-06-29 12:42:32 +0530

    ui updates

--- lib/screens/calculator/user_inputs.dart
+++ lib/screens/calculator/user_inputs.dart
@@ -42,108 +42,128 @@ class _UserInputsState extends State<UserInputs> {
     }
 
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              color:ColorPallete.background,
-            ),
-
-            if(args == 'food')
-            FlareActor(
-              'assets/flare/foodstash_f_0.flr',
-              animation: 'Flow',
-            ),
-
-            if(args == 'travel')
-            FlareActor(
-              'assets/flare/foodstash_f_0.flr',
-              animation: 'Flow',
-            ),
-            
-            if(args == 'water')
-            FlareActor(
-              'assets/flare/foodstash_f_0.flr',
-              animation: 'Flow',
-            ),
-
-            Positioned(
-              child: Container(
-                height: size.height,
-                width: size.width,
-                //decoration: BoxDecoration(color: ColorPallete.background),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Align(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                        child: Text(
-                          questions[index],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: ColorPallete.color3,
-                            fontSize: 20,
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          child: Stack(
+            children: [
+              Container(
+                color: ColorPallete.background,
+              ),
+              
+              if (args == 'food')
+                FlareActor(
+                  'assets/flare/foodstash_f_0.flr',
+                  animation: 'Flow',
+                ),
+              if (args == 'travel')
+              FlareActor(
+                'assets/flare/base_two.flr',
+                animation: 'island01',
+                color: ColorPallete.color3,
+                fit: BoxFit.cover,
+              ),
+                if (index == 0)
+                  FlareActor(
+                    'assets/flare/watch_tv.flr',
+                    animation: 'flow',
+                    alignment: Alignment.bottomCenter,
+                  ),
+                  if (index == 1)
+                  FlareActor(
+                    'assets/flare/tv.flr',
+                    animation: 'flow',
+                    alignment: Alignment.bottomCenter,
+                  ),
+                  if (index == 2)
+                  FlareActor(
+                    'assets/flare/watch_tv.flr',
+                    animation: 'flow',
+                    alignment: Alignment.bottomCenter,
+                  ),
+              if (args == 'water')
+                FlareActor(
+                  'assets/flare/foodstash_f_0.flr',
+                  animation: 'Flow',
+                ),
+              Positioned(
+                child: Container(
+                  height: size.height,
+                  width: size.width,
+                  //decoration: BoxDecoration(color: ColorPallete.background),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                          child: Text(
+                            questions[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: ColorPallete.color3,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Align(
-                      child: Container(
-                        width: size.width * 0.4,
-                        child: TextFormField(
-                          controller: _answerController,
-                          decoration: InputDecoration(
-                            //enabledBorder: InputBorder.none,
-                            //filled: true,
-                            //fillColor: ColorPallete.background.withBlue(-200),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                Icons.trending_flat,
-                                color: ColorPallete.color4,
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Align(
+                        child: Container(
+                          width: size.width * 0.4,
+                          child: TextFormField(
+                            controller: _answerController,
+                            decoration: InputDecoration(
+                              //enabledBorder: InputBorder.none,
+                              //filled: true,
+                              //fillColor: ColorPallete.background.withBlue(-200),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.trending_flat,
+                                  color: ColorPallete.color4,
+                                ),
+                                onPressed: () {
+                                  if (_answerController.text == '' ||
+                                      _answerController is String) {
+                                    return;
+                                  }
+                                  answers.add(_answerController.text);
+                                  if (index == questions.length - 1) {
+                                    Navigator.of(context).pushReplacementNamed(
+                                      ResultScreen.routeName,
+                                      arguments: answers,
+                                    );
+                                  } else {
+                                    index++;
+                                    print(index);
+                                  }
+                                  setState(() {});
+                                },
                               ),
-                              onPressed: () {
-                                if (_answerController.text == '' ||
-                                    _answerController is String) {
-                                  return;
-                                }
-                                answers.add(_answerController.text);
-                                if (index == questions.length - 1) {
-                                  Navigator.of(context).pushReplacementNamed(
-                                    ResultScreen.routeName,
-                                    arguments: answers,
-                                  );
-                                } else {
-                                  index++;
-                                  print(index);
-                                }
-                                setState(() {});
-                              },
+                              alignLabelWithHint: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 2,
+                              ),
                             ),
-                            alignLabelWithHint: true,
-                            contentPadding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 2,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.text,
+                            style: TextStyle(
+                              color: ColorPallete.color3,
                             ),
-                          ),
-                          textAlign: TextAlign.center,
-                          keyboardType: TextInputType.text,
-                          style: TextStyle(
-                            color: ColorPallete.color3,
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
